ad3803f19eb04838e2188651972ed06f5eb74ad6
Farook13 <umarulfarook787@gmail.com>
2025-06-15 02:03:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import logging
 import logging.config
 
-Get logging configurations
+#Get logging configurations
 
 logging.config.fileConfig('logging.conf')
 logging.getLogger().setLevel(logging.INFO)
