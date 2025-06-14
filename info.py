f351107c693bad012d3f0669156ec29290ac5965
Farook13 <umarulfarook787@gmail.com>
2025-06-15 02:34:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -46,7 +46,7 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 support_chat_id = environ.get('SUPPORT_CHAT_ID')
 reqst_channel = environ.get('REQST_CHANNEL_ID')
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
-SUPPORT_CHAT_ID =-1002332361885
+SUPPORT_CHAT_ID =-100233500266
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
@@ -59,7 +59,7 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 MAX_B_TN = environ.get("MAX_B_TN", "10")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
 LOG_CHANNEL = environ.get('LOG_CHANNEL', 'hcjdhdhxidjshudhchd')  # no @ symbol
-SUPPORT_CHAT = environ.get("SUPPORT_CHAT", "hcjdhdhxidjshudhchd")  # Or any fallback username
+SUPPORT_CHAT = environ.get("SUPPORT_CHAT", "+55TIaLUzEQAwNjhl")  # Or any fallback username
  # e.g. "my_support_group"
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), False)
 IMDB = is_enabled((environ.get('IMDB', "False")), True)
