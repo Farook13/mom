1d79e02fbcfaf7b752ad1d4bc6835a9b7d4baa44
Farook13 <umarulfarook787@gmail.com>
2025-06-14 23:23:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -29,9 +29,9 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 BOT_START_TIME = time()
 
 # Bot images & videos
-PICS = (environ.get('PICS', 'https://envs.sh/Mll.jpg')).split()
+PICS = (environ.get('PICS', 'https://envs.sh/Mkp.mp4')).split()
 NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/0593a3103ba1b9a5855bf.jpg")
-MELCOW_VID = environ.get("MELCOW_VID", "https://graph.org/file/72dff2b65352ba85d0a34.mp4")
+MELCOW_VID = environ.get("MELCOW_VID", "https://envs.sh/Mkp.mp4")
 SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/2a888a370f479f4338f7c.jpg")
 
 # Admins, Channels & Users
