e2d405ba4955ea43f41dd287f9452fceb6d74ed1
Farook13 <umarulfarook787@gmail.com>
2025-06-15 19:02:09 +0530

    Update info.py

--- info.py
+++ info.py
@@ -50,8 +50,8 @@ SUPPORT_CHAT_ID = "cine_flix01"
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://batman13:batman13@batman.sawvl.mongodb.net/?retryWrites=true&w=majority&appName=batman")
-DATABASE_NAME = environ.get('DATABASE_NAME', "batman")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://death:death@cluster0.evhwgcz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "death")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'TELEGRAM_FILES')
 
 # Others
